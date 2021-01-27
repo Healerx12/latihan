840b054a729485b6a6791a72230440af54d93582
Healerx12 <48481462+Healerx12@users.noreply.github.com>
2021-01-27 15:37:55 +0800

    Update App.js

--- pages/App.js
+++ pages/App.js
@@ -1,31 +1,42 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-// import SplashScreen from './src/pages/splashscreen';
-import Register from './src/pages/Register';
-import Login from './src/pages/login';
-import Home from './src/pages/home';
-import profil from './src/pages/home';
+import React from 'react'
+import { StatusBar } from 'react-native'
+import { Provider } from 'react-native-paper'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { theme } from './src/core/theme'
+import {
+  StartScreen,
+  LoginScreen,
+  RegisterScreen,
+  ForgotPasswordScreen,
+  Dashboard,
+} from './src/screens'
+
+StatusBar.setBarStyle('light-content')
+const Stack = createStackNavigator()
+
+const App = () => {
+  return (
+    <Provider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="StartScreen"
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="StartScreen" component={StartScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+          <Stack.Screen
+            name="ForgotPasswordScreen"
+            component={ForgotPasswordScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
+}
 
 
-
-
- const AuthStack = createStackNavigator(
- {
-  login: {screen : Login},
-  Register: {screen : Register},
-  profil: {screen:profil}
- 
- 
-  
- });
-
- const MainStack = createStackNavigator({
-  Home: {screen: Home}
- });
-
- const Navigators = createSwitchNavigator({
-  Auth: { screen: AuthStack},
-  Main: { screen: MainStack}
- });
-
- export default createAppContainer(Navigators);
